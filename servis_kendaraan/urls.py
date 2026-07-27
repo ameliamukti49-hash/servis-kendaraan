44a3a63d019cc1095a688f29fd6a3e962293bd7b
Laily225 <hafidahramadanilaili@gmail.com>
2026-07-27 13:45:55 +0700

    Menambahkan modul mekanik dan work order

--- servis_kendaraan/urls.py
+++ servis_kendaraan/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', include('dashboard.urls')),
+    path('mekanik/', include('mekanik.urls')),
 ]
