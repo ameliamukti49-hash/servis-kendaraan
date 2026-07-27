22fa50ed04dc733c6fd3ec0bb6cc63cbcc00ffea
ameliamukti <ameliamukti49@gmail.com>
2026-07-27 14:38:48 +0700

    Memperbarui konfigurasi project dan migrasi akun

--- servis_kendaraan/urls.py
+++ servis_kendaraan/urls.py
@@ -3,6 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Accounts
     path('', include('accounts.urls')),
+
+    # Dashboard
     path('', include('dashboard.urls')),
+
+    # Booking & Kendaraan
+    path('', include('booking.urls')),
 ]
